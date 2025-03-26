e0b418c69c31cf18b88254e96d0040ed0cab58b8
AgostonAttila <atesz014@gmail.com>
2025-03-26 10:03:16 +0100

    add Frontend

--- Eshop MicroServices/Services/Ordering/Ordering.API/Program.cs
+++ Eshop MicroServices/Services/Ordering/Ordering.API/Program.cs
@@ -1,26 +1,37 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Ordering.API;
 using Ordering.Application;
 using Ordering.Infrastructure;
 using Ordering.Infrastructure.Data.Extensions;
 
-var builder = WebApplication.CreateBuilder(args);
-
-
-// Add services to the container.
-builder.Services
-    .AddApplicationServices(builder.Configuration)
-    .AddInfrastructureServices(builder.Configuration)
-    .AddApiServices(builder.Configuration);
-
-var app = builder.Build();
-
-
-// Configure the HTTP request pipeline.
-app.UseApiServices();
-
-if (app.Environment.IsDevelopment())
+namespace Ordering.API
 {
-    await app.InitialiseDatabaseAsync();
-}
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-app.Run();
+            // Add services to the container.
+            builder.Services
+                .AddApplicationServices(builder.Configuration)
+                .AddInfrastructureServices(builder.Configuration)
+                .AddApiServices(builder.Configuration);
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            app.UseApiServices();
+
+            if (app.Environment.IsDevelopment())
+            {
+                await app.InitialiseDatabaseAsync();
+            }
+
+            app.Run();
+        }
+    }
+}
